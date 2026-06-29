ac7a11766d3a1000af59b8408f039274988d7c65
riyamanek <manekriya4@gmail.com>
2026-06-29 11:16:21 +0530

    Add files via upload

---
+++ assignments/Module2/task/sqaure cube.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main (){
+
+int num= 5;
+
+int sqaure=num*num;
+printf("\n num of sqaure is :- %d",sqaure);
+
+int cube= num*num*num;
+printf("\n num of cube is :- %d",cube);
+
+getch();
+
+}

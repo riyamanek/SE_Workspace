8896c093e8f7c54bd0f627f4ea76b718661f72bb
riyamanek <manekriya4@gmail.com>
2026-06-29 11:30:58 +0530

    Add files via upload

---
+++ assignments/Module2/task/stu per.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main (){
+    
+int sub1=65;
+int sub2=78;
+int sub3=88;
+int sub4=95;
+int sub5=89;
+
+int sum =sub1+sub2+sub3+sub4+sub5;
+printf("\n Sum of 5 sub markes :- %d",sum);
+
+float per =sum/5;
+printf("\n Percentage is :- %f",per);
+
+getch();
+}

678c85d4f7f90f12d409a958d035b90f86526e53
riyamanek <manekriya4@gmail.com>
2026-06-29 11:42:46 +0530

    Delete assignments/Module2/task/print information.c

--- assignments/Module2/task/print information.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <conio.h>
-void main(){
-    printf(" \n Name :-  Riya manek");
-    printf("\n Birthdate :- 01/08/2006");
-    printf("\n Age :- 19");
-    printf("\n Address :- Shreenath App, Avenyu Park,Ravapar Road,Morbi");
-    getch();
-}

776d0651ae94006856bdf80720d52c33ce012ee0
riyamanek <manekriya4@gmail.com>
2026-06-29 11:18:21 +0530

    Add files via upload

---
+++ assignments/Module2/task/area of circle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h>
+void main(){
+    int r=5;
+    float PI= 3.14;
+    
+    float area=PI*r*r;
+    printf("Area of circle is :- %f",area);
+    
+    getch();
+}

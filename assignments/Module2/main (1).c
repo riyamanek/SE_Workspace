ed79b0171f84792b3a0459f1ebd37da7f6fce8f0
riyamanek <manekriya4@gmail.com>
2026-06-29 11:03:58 +0530

    Delete assignments/Module2/main (1).c

--- assignments/Module2/main (1).c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<conio.h>
-void main(){
-    printf("\nName :- Riya Manek");
-    printf("\nCity :- Morbi");
-    printf("\nBirth Date :- 01/08/2006");
-    printf("\nDegree :- BCA");
-    getch();
-}
